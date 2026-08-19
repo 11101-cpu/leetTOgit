52b31bc6f6e12bd07a07aadc2c13a5e326effd29
Samron Trevor Miranda <samronmiranda@gmail.com>
2026-08-19 22:24:00 +0530

    Time: 3 ms (86.09%) | Memory: 43.3 MB (41.34%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.java
@@ -0,0 +1,51 @@
+import java.util.Stack;
+
+class Solution {
+    public boolean isValid(String s) {
+
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i < s.length(); i++) {
+
+            char ch = s.charAt(i);
+
+            // Opening brackets
+            if (ch == '(' || ch == '{' || ch == '[') {
+                stack.push(ch);
+            }
+
+            // Closing brackets
+            else {
+
+                if (stack.isEmpty()) {
+                    return false;
+                }
+
+                char top = stack.pop();
+
+                if (ch == ')' && top != '(') {
+                    return false;
+                }
+
+                if (ch == '}' && top != '{') {
+                    return false;
+                }
+
+                if (ch == ']' && top != '[') {
+                    return false;
+                }
+            }
+        }
+
+        return stack.isEmpty();
+    }
+}
+public class Main {
+    public static void main(String[] args) {
+
+        Solution sol = new Solution();
+        String Pf = "[4*(3+2)]";
+       sol.isValid(Pf);
+
+    }
+}
