14b8f7ab716813174cdd126f2c7a81501553b964
Samron Trevor Miranda <samronmiranda@gmail.com>
2026-09-17 21:57:13 +0530

    Time: 4 ms (100.00%) | Memory: 45.7 MB (93.66%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.java
@@ -0,0 +1,35 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        int reversed = 0;
+        int y = x;
+        if (x > 0) {
+            while (x != 0) {
+                int digit = x % 10;
+                reversed = reversed * 10 + digit;
+                x /= 10;
+            }
+        }
+        if (y == reversed) {
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+}
+class Main {
+    public static void main(String[] args) {
+
+        boolean result;
+        Solution sol = new Solution();
+
+        result = sol.isPalindrome(10);
+        if(result==true){
+            System.out.println("Is A Palindrome ");
+        }
+        else{
+            System.out.println("Is Not A Palindrome");
+        }
+
+    }
+}
